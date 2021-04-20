20f90bec6e0ad290ec08a90c7fcbfdb60f011ce6
Black <123485506@qq.com>
2021-04-20 16:18:03 +0800

    初始化YLBasedComponent框架

---
+++ Example/Pods/Target Support Files/Pods-YLBasedComponent_Tests/Pods-YLBasedComponent_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YLBasedComponent_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YLBasedComponent_Tests
+@end
---
+++ Example/Pods/Target Support Files/YLBasedComponent/YLBasedComponent-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YLBasedComponent : NSObject
+@end
+@implementation PodsDummy_YLBasedComponent
+@end
